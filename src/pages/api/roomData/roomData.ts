2a4b5631f82c5ed295e55d3a8961f948a0347632
kinako <jsva4127@yahoo.co.jp>
2023-05-08 00:34:17 +0900

    add data.tsファイル

---
+++ src/pages/api/roomData/roomData.ts
@@ -0,0 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { supabase } from '../../lib/supabase';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    const { data } = req.body;
+
+    const { error } = await supabase.from('rooms').insert(data);
+
+    if (error) {
+      res.status(500).json({ error: error.message });
+    } else {
+      res.status(200).json({ message: 'Data saved successfully' });
+    }
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
+  }
+}
