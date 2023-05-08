b0878c25305bfc62351f5b72e443a0dd40dfc4c3
kinako <jsva4127@yahoo.co.jp>
2023-05-08 23:47:31 +0900

    fix CI

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withTM = require('next-transpile-modules')(['gridstack']);
 
-module.exports = nextConfig
+module.exports = withTM({
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // 他の必要なカスタマイズを追加
+
+    return config;
+  },
+});
